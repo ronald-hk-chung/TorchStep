2dab98450f209d166be64ddfc724926b4f882d4f
ronald-hk-chung <hoi.k.chung@gmail.com>
2024-02-18 16:39:28 +0800

    update

--- engine.py
+++ engine.py
@@ -517,19 +517,19 @@ class TSEngine:
 
 
   class Callback:
-    def __init__(self, **kwargs): pass
-    def on_train_begin(self, **kwargs): pass
-    def on_train_end(self, **kwargs): pass
-    def on_valid_begin(self, **kwargs): pass
-    def on_valid_end(self, **kwargs): pass
-    def on_epoch_begin(self, **kwargs): pass
-    def on_epoch_end(self, **kwargs): pass
-    def on_batch_begin(self, **kwargs): pass
-    def on_batch_end(self, **kwargs): pass
-    def on_loss_begin(self, **kwargs): pass
-    def on_loss_end(self, **kwargs): pass
-    def on_step_begin(self, **kwargs): pass
-    def on_step_end(self, **kwargs): pass
+    def __init__(self): pass
+    def on_train_begin(self): pass
+    def on_train_end(self): pass
+    def on_valid_begin(self): pass
+    def on_valid_end(self): pass
+    def on_epoch_begin(self): pass
+    def on_epoch_end(self): pass
+    def on_batch_begin(self): pass
+    def on_batch_end(self): pass
+    def on_loss_begin(self): pass
+    def on_loss_end(self): pass
+    def on_step_begin(self): pass
+    def on_step_end(self): pass
 
   class callback_handler:
     def on_train_begin(self):
