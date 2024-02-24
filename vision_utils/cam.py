f574a9cb4ff968a2f095dd1bfa839c2456768430
ronald-hk-chung <hoi.k.chung@gmail.com>
2024-02-24 22:25:19 +0800

    update

--- vision_utils/cam.py
+++ vision_utils/cam.py
@@ -1,5 +1,4 @@
 import torch
-import torchvision
 from torchvision.transforms.functional import to_pil_image
 from torchvision import transforms
 from torchvision.transforms import v2 as T
@@ -9,65 +8,70 @@ from matplotlib import colormaps
 from typing import Callable
 import numpy as np
 
-def get_cam(img_path: str,
-            tfms: transforms.Compose,
-            rtfms: transforms.Compose,
-            classifier: Callable,
-            layers_to_activate: list,
-            class_to_activate: int):
-  """
-  Method to get Class Activation Maps(CAM) for in image
-  
-  Args:
-    img_path [str]: path of img to be analysed
-    tfms [transforms]: transform of image, PIL to Tensor
-    rtfms [transforms]: reverse transform of image, Tensor to PIL
-    classifier [TSEngine]: TSEngine class classifier
-    layers_to_activate [list]: layers to get activation from model in classifier
-    class_to_activate [int]: class for Class Activation Maps (CAM) analysis
-  """
-  gradients = None
-  activations = None
 
-  def backward_hook(module, grad_input, grad_output):
-    nonlocal gradients
-    gradients = grad_output
+def get_cam(
+    img_path: str,
+    tfms: transforms.Compose,
+    rtfms: transforms.Compose,
+    classifier: Callable,
+    layers_to_activate: list,
+    class_to_activate: int,
+):
+    """
+    Method to get Class Activation Maps(CAM) for in image
 
-  def forward_hook(module, args, output):
-    nonlocal activations
-    activations = output
-  
-  # unfreeze layers and attach hooks to layers_to_activate
-  classifier.unfreeze()
-  classifier.attach_forward_hooks(layers_to_activate, forward_hook)
-  classifier.attach_backward_hooks(layers_to_activate, backward_hook)
+    Args:
+      img_path [str]: path of img to be analysed
+      tfms [transforms]: transform of image, PIL to Tensor
+      rtfms [transforms]: reverse transform of image, Tensor to PIL
+      classifier [TSEngine]: TSEngine class classifier
+      layers_to_activate [list]: layers to get activation from model in classifier
+      class_to_activate [int]: class for Class Activation Maps (CAM) analysis
+    """
+    gradients = None
+    activations = None
 
-  # Open image and perform forward and backward pass on class to activates
-  img = tfms(Image.open(img_path).convert('RGB'))
-  y_logits = classifier.model(img.unsqueeze(dim=0).to('cuda'))
-  print(f'Prediction: {y_logits}')
-  y_logits[class_to_activate].backward()
+    def backward_hook(module, grad_input, grad_output):
+        nonlocal gradients
+        gradients = grad_output
 
-  # freeze classifier and remove hooks
-  classifier.remove_hooks()
-  classifier.freeze()
+    def forward_hook(module, args, output):
+        nonlocal activations
+        activations = output
 
-  # Calculate gradient heatmap
-  pooled_gradients = torch.mean(gradients[0], dim=[0, 2, 3])
-  for i in range(activations.size()[1]):
-    activations[:, i, : :] *= pooled_gradients[i]
-  heatmap = torch.mean(activations, dim=1).squeeze()
-  heatmap = torch.nn.functional.relu(heatmap)
-  heatmap /= torch.max(heatmap)
+    # unfreeze layers and attach hooks to layers_to_activate
+    classifier.unfreeze()
+    classifier.attach_forward_hooks(layers_to_activate, forward_hook)
+    classifier.attach_backward_hooks(layers_to_activate, backward_hook)
 
-  # Plot out original image and image with heatmap
-  fig, (ax1, ax2) = plt.subplots(1,2)
-  ax1.imshow(rtfms(img))
-  ax1.axis(False)
-  overlay = to_pil_image(heatmap.detach(), mode='F').resize((img.shape[1],img.shape[2]), resample=Image.BICUBIC)
-  cmap = colormaps['jet']
-  overlay = (255 * cmap(np.asarray(overlay) ** 2)[:, :, :3]).astype(np.uint8)
-  ax2.imshow(rtfms(img))
-  ax2.imshow(overlay, alpha=0.4, interpolation='nearest')
-  ax2.axis(False)
-  plt.show()
+    # Open image and perform forward and backward pass on class to activates
+    img = tfms(Image.open(img_path).convert("RGB"))
+    y_logits = classifier.model(img.unsqueeze(dim=0).to("cuda"))
+    print(f"Prediction: {y_logits}")
+    y_logits[class_to_activate].backward()
+
+    # freeze classifier and remove hooks
+    classifier.remove_hooks()
+    classifier.freeze()
+
+    # Calculate gradient heatmap
+    pooled_gradients = torch.mean(gradients[0], dim=[0, 2, 3])
+    for i in range(activations.size()[1]):
+        activations[:, i, ::] *= pooled_gradients[i]
+    heatmap = torch.mean(activations, dim=1).squeeze()
+    heatmap = torch.nn.functional.relu(heatmap)
+    heatmap /= torch.max(heatmap)
+
+    # Plot out original image and image with heatmap
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.imshow(rtfms(img))
+    ax1.axis(False)
+    overlay = to_pil_image(heatmap.detach(), mode="F").resize(
+        (img.shape[1], img.shape[2]), resample=Image.BICUBIC
+    )
+    cmap = colormaps["jet"]
+    overlay = (255 * cmap(np.asarray(overlay) ** 2)[:, :, :3]).astype(np.uint8)
+    ax2.imshow(rtfms(img))
+    ax2.imshow(overlay, alpha=0.4, interpolation="nearest")
+    ax2.axis(False)
+    plt.show()
--- vision_utils/utils.py
+++ vision_utils/utils.py
@@ -1,66 +1,69 @@
-import torch
-import torchvision
 from torchvision import transforms
 from torchvision.transforms import v2 as T
 from torch.utils.data import DataLoader
-from PIL import Image
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from typing import Callable
 import numpy as np
 
 
-def show_batch(dataloader: torch.utils.data.DataLoader,
-               labelling: Callable,
-               transforms: transforms.Compose = T.ToPILImage()):
-  """Function to show a batch of images with custom labelling
-  
-  Args:
-    dataloader [DataLoader]: DataLoader of images to show
-    transforms [transforms]: transform of image tensor to PIL
-    labelling [Callable]: function to return label given labels generated from dataloader
-  """
-  fig = plt.figure(figsize=(20, 10))
-  imgs, *labels = next(iter(dataloader))
-  nrows, ncolumns = 4, 8
-  for i, label in enumerate(list(zip(*labels))):
-    plt.subplot(nrows, ncolumns, i + 1)
-    plt.imshow(transforms(imgs[i]))
-    plt.title(labelling(label))
+def show_batch(
+    dataloader: DataLoader,
+    labelling: Callable,
+    transforms: transforms.Compose = T.ToPILImage(),
+):
+    """Function to show a batch of images with custom labelling
+
+    Args:
+      dataloader [DataLoader]: DataLoader of images to show
+      transforms [transforms]: transform of image tensor to PIL
+      labelling [Callable]: function to return label given labels generated from dataloader
+    """
+    fig = plt.figure(figsize=(20, 10))
+    imgs, *labels = next(iter(dataloader))
+    nrows, ncolumns = 4, 8
+    for i, label in enumerate(list(zip(*labels))):
+        plt.subplot(nrows, ncolumns, i + 1)
+        plt.imshow(transforms(imgs[i]))
+        plt.title(labelling(label))
+        plt.axis(False)
+
+
+def show_image(
+    img: np.ndarray,
+    bbs: list = None,
+    bbs_format: str = "XYXY",
+    bbs_label: list = None,
+    color: str = "r",
+):
+    """Show Image
+
+    Args:
+      img [ndarray]: image to show
+      bbs [list]: list bounding boxes
+      bbs_format [str]: bounding box format ('XYXY', 'XYWH', 'CXCYWH'), default to 'XYXY'
+      bbs_label [list]: list of bounding box label
+      color [str]: edgecolor of bounding box
+    """
+    fig, ax = plt.subplots()
+    ax.imshow(img)
+    if bbs is not None:
+        for i, bb in enumerate(bbs):
+            if bbs_format == "XYXY":
+                x1, y1, x2, y2 = bb
+                w, h = x2 - x1, y2 - y1
+            elif bbs_format == "XYWH":
+                x1, y1, w, h = bb
+            elif bbs_format == "CXCYWH":
+                cx, cy, w, h = bb
+                x1 = cx - w / 2
+                y1 = cy - h / 2
+            rect = patches.Rectangle(
+                (x1, y1), w, h, linewidth=1, edgecolor=color, facecolor="none"
+            )
+            ax.add_patch(rect)
+            if bbs_label is not None:
+                ax.annotate(bbs_label[i], (x1, y1), color=color)
+
     plt.axis(False)
-
-
-def show_image(img: np.ndarray,
-               bbs: list = None,
-               bbs_format: str = 'XYXY',
-               bbs_label: list = None,
-               color: str = 'r'):
-  """Show Image
-
-  Args:
-    img [ndarray]: image to show
-    bbs [list]: list bounding boxes
-    bbs_format [str]: bounding box format ('XYXY', 'XYWH', 'CXCYWH'), default to 'XYXY'
-    bbs_label [list]: list of bounding box label
-    color [str]: edgecolor of bounding box
-  """
-  fig, ax = plt.subplots()
-  ax.imshow(img)
-  if bbs is not None:
-    for i, bb in enumerate(bbs):
-      if bbs_format == 'XYXY':
-        x1, y1, x2, y2 = bb
-        w, h = x2-x1, y2-y1
-      elif bbs_format == 'XYWH':
-        x1, y1, w, h = bb
-      elif bbs_format == 'CXCYWH':
-        cx, cy, w, h = bb
-        x1 = cx - w/2
-        y1 = cy - h/2
-      rect = patches.Rectangle((x1, y1), w, h, linewidth=1, edgecolor=color, facecolor='none')
-      ax.add_patch(rect)
-      if bbs_label is not None:
-        ax.annotate(bbs_label[i], (x1, y1), color=color)
-
-  plt.axis(False)
-  plt.show()
+    plt.show()
