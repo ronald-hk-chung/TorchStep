446d44d21e95bb783f4bef076865d6b2f3918a61
ronald-hk-chung <hoi.k.chung@gmail.com>
2024-02-24 22:48:10 +0800

    update to_device

--- engine/device_handler.py
+++ engine/device_handler.py
@@ -5,11 +5,11 @@ from typing import Any
 def to_device(X: Any, device: str):
     """Method to put variable X to gpu if available"""
     if isinstance(X, list):
-        return [to_device(x) for x in X]
+        return [to_device(x, device) for x in X]
     elif isinstance(X, tuple):
-        return tuple(to_device(x) for x in X)
+        return tuple(to_device(x, device) for x in X)
     elif isinstance(X, dict):
-        return {k: to_device(x) for k, x in X.items()}
+        return {k: to_device(x, device) for k, x in X.items()}
     elif torch.is_tensor(X):
         return X.to(device)
     else:
