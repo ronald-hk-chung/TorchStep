e187b3e71fab24d667d02e720d9a177b20692ce3
ronald-hk-chung <hoi.k.chung@gmail.com>
2024-05-21 22:13:46 +0800

    googlenet

--- src/sstorch/vision/__init__.py
+++ src/sstorch/vision/__init__.py
@@ -3,6 +3,7 @@ from .lenet import LeNet5
 from .alexnet import AlexNet
 from .vgg import VGGNet
 from .resnet import ResNet
+from .googlenet import GoogleNet
 from .cam import get_cam
 from .models import get_pretrained_model
 from .selective_search import SelectiveSearch
